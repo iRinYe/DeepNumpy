996b2d40b659fbf49ce419c6655b19fe2c137460
iRinYe <YeYilinCN@outlook.com>
2020-05-25 11:48:29 +0800

    Conv2d会比PyTorch慢大约三倍, 可能是因为多次for循环导致的

---
+++ CNN.py
@@ -0,0 +1,65 @@
+# @Time    : 2020/5/22 11:36
+# @Author  : iRinYe
+# @Email   : YeYilinCN@outlook.com
+# @File    : FC
+# @Software: PyCharm
+
+"""
+    利用Numpy实现CNN
+    PyTorch AUC: 0.9485, Time used:0.0033s
+    DeepNumpy AUC: 0.9485, Time used:0.00943s
+"""
+import time
+
+import torch
+from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import roc_auc_score
+
+import DeepNumpy
+from lib import getDataLoader, train, test, getModelWeight
+
+
+class Model(torch.nn.Module):
+    # 模型定义
+    def __init__(self):
+        super(Model, self).__init__()
+        self.CNN = torch.nn.Conv2d(in_channels=1, out_channels=5, kernel_size=3)
+        self.FC = torch.nn.Linear(in_features=5 * 3 * 4, out_features=2)
+
+    def forward(self, x):
+        temp = self.CNN(x)
+        temp = self.FC(temp.view(-1, 5 * 3 * 4))
+        return torch.sigmoid(temp)
+
+
+if __name__ == "__main__":
+    # 主函数入口
+    x, y = load_breast_cancer(True)
+    train_len = int(len(x) / 10 * 9)
+    dl = getDataLoader(x[:train_len].reshape(-1, 1, 5, 6), y[:train_len], 30, True)
+    model = Model()
+
+    model = train(model=model, dataloader=dl, EPOCH=30, loss="CEP")
+
+    # PyTorch Test
+    dl = getDataLoader(x[train_len:].reshape(-1, 1, 5, 6), y[train_len:], 30, False)
+    start = time.perf_counter()
+    result = test(model, dl)
+    auc = roc_auc_score(y[train_len:] == 1, result)
+    print()
+    print()
+    print()
+    print("PyTorch AUC: {}, Time used:{}s".format(round(auc, 5), round(time.perf_counter() - start, 5)))
+
+    # Numpy Test
+    start = time.perf_counter()
+    weight_dict = getModelWeight(model)
+
+    temp = DeepNumpy.Conv2d(x[train_len:].reshape(-1, 5, 6), weight_dict['CNN.weight'], weight_dict['CNN.bias'])
+    temp = DeepNumpy.Flatten(temp)
+    temp = DeepNumpy.Linear(temp, weight_dict['FC.weight'], weight_dict['FC.bias'])
+    temp = DeepNumpy.Sigmoid(temp)
+
+    result = temp[:, 1].reshape(-1, 1)
+    auc = roc_auc_score(y[train_len:] == 1, result)
+    print("DeepNumpy AUC: {}, Time used:{}s".format(round(auc, 5), round(time.perf_counter() - start, 5)))
--- DeepNumpy.py
+++ DeepNumpy.py
@@ -23,40 +23,53 @@ def Tanh(x):
     return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
 
+def Flatten(x):
+    return x.reshape(x.shape[0], -1)
+
+
 def Linear(x, weight, bias):
     return np.dot(x, weight.T) + bias
 
 
-def Conv2d(x, weight):
-    # todo 以下参数暂时为超参
-    kernel_size = (3, 3)
-    stride = (1, 1)
-    padding = 0
-    channel = 1
+def Conv2d(x, CNN_filter, CNN_bias, stride=(1, 1), padding=0):
+    """
+    二维卷积
+    :param x: 输入矩阵[batch, channel, H, W]
+    :param CNN_filter: 卷积滤片的权重
+    :param CNN_bias: 卷积滤片的偏置
+    :param stride: 步长, 默认为(1, 1)
+    :param padding: 填充, 默认为(1, 1), 该参数暂时闲置
+    :return: 卷积后的结果数组[batch, filter, channel, H, W]
+    """
+    kernel_size = (CNN_filter.shape[2], CNN_filter.shape[3])
+    channel = CNN_filter.shape[1]
 
     input_shape = x.shape
     FM_size = (input_shape[1], input_shape[2])
 
-    Fh = int((FM_size[0] - kernel_size[0] + 2 * padding) / stride[0] + 1)       # 横向滑动次数
-    Fw = int((FM_size[1] - kernel_size[1] + 2 * padding) / stride[1] + 1)       # 纵向滑动次数
-
-    CNN_filter = weight['CNN.weight']
-    CNN_bias = weight['CNN.bias']
+    Fh = int((FM_size[0] - kernel_size[0] + 2 * padding) / stride[0] + 1)
+    Fw = int((FM_size[1] - kernel_size[1] + 2 * padding) / stride[1] + 1)
 
     x = x.reshape(input_shape[0], channel, input_shape[1], input_shape[2])
-    feature_map_List = []
+
+    b_result = None
+
     for b in range(input_shape[0]):
         # 一批中的第b个样本
-        for c in range(channel):
-            # 第c个channel
-            for f in range(CNN_filter.shape[0]):
-                # 第f张滤片
-                for i in range(Fw):
-                    # 横向滑动
-                    for j in range(Fh):
-                        # 纵向滑动
-                        # todo 整不明白了...
-                        current_field = x[b, c, i: i+kernel_size[0], j: j+kernel_size[1]]
-                        feature_map = np.sum(np.dot(current_field, CNN_filter[f, c, :, :]))
-                        feature_map_List.append(feature_map)
-        pass
+        # todo 碰到多个channel的情况还没测试 感觉Channel维度的运算可能有问题
+        feature_map = None
+        for i in range(Fh):  # row start index
+            row_temp = None
+            for j in range(Fw):  # col start index
+
+                current_field = x[b, :, i: i + kernel_size[0], j: j + kernel_size[1]]  # [channel, h, w]
+                temp = np.multiply(current_field, CNN_filter)
+                temp = np.sum(temp, axis=(2, 3))  # [batch, filter num, channel, 1, 1]
+                temp = temp + CNN_bias.reshape(temp.shape)  # [batch, filter num, channel, 1, 1]
+                temp = temp.reshape(1, CNN_filter.shape[0], CNN_filter.shape[1], 1, 1)
+
+                row_temp = np.concatenate((row_temp, temp), axis=-1) if row_temp is not None else temp
+            feature_map = np.concatenate((feature_map, row_temp), axis=-2) if feature_map is not None else row_temp
+        b_result = np.concatenate((b_result, feature_map), axis=0) if b_result is not None else feature_map
+
+    return b_result
